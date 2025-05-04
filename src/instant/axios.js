64f582c475745f28d944968adf8015a7eb5388f6
Ankesh1A <ankeshbarahdiya05112002@gmail.com>
2025-05-04 22:02:27 +0530

    dsfv

--- src/instant/axios.js
+++ src/instant/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
    baseURL: 'https://back1-0dbz.onrender.com',
-  // baseURL: 'http://localhost:8080',  Backend URL
+ 
   headers: {
     'Content-Type': 'application/json',
   },
